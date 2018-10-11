a4f58beff3a793baf0cbc1732fb3d63bbfbdac94
Daniel Corn <cod@iresults.li>
2018-10-11 16:52:27 +0200

    Code formatting

--- src/TypeDecorator.tsx
+++ src/TypeDecorator.tsx
@@ -1,4 +1,6 @@
 import 'reflect-metadata';
 import { ClassConstructorType } from './ClassConstructorType';
 
-export function Type<T>(type: ClassConstructorType<T>) { return Reflect.metadata('design:type', type); }
+export function Type<T>(type?: ClassConstructorType<T>) {
+    return Reflect.metadata('design:type', type);
+}
